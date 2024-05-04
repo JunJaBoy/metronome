14ed0340df1036c0378e4a782ad1d0b74f4623d3
junsuPark <krpjs0508@gmail.com>
2024-05-04 14:41:15 +0900

    bpm meter

--- lib/core/bpm/bpm_meter.dart
+++ lib/core/bpm/bpm_meter.dart
@@ -1,30 +1,71 @@
-/*
-abstract class BpmMeter {
-  int beat;
-  Stream<int> bpmStream;
+import 'dart:async';
+
+import 'package:metronome/core/bpm/bpm_util.dart';
+
+class BpmMeter {
+  final StreamController<int> _beatController = StreamController();
+
+  Stream<int> get beatStream => _beatController.stream;
+  int _beatInternal = 1;
+
+  int _targetBeat;
+
+  int get targetBeat => _targetBeat;
+
+  int _bpm;
+
+  int get bpm => _bpm;
+
+  late Timer _timer;
 
   BpmMeter({
-    required this.beat,
-    required this.bpmStream,
-  });
+    int targetBeat = 4,
+    int bpm = 68,
+  })  : _bpm = bpm,
+        _targetBeat = targetBeat {
+    if (_targetBeat < 0) {
+      throw const FormatException("Illegal initial beat");
+    }
+  }
 
-  void play();
+  void resume() {
+    _timer = Timer.periodic(
+      Duration(milliseconds: BpmUtil.bpmToMillis(_bpm)),
+      (timer) => _tick(),
+    );
+  }
 
-  void pause();
+  void _tick() {
+    _beatController.add(_calculateBeat());
+  }
+
+  int _calculateBeat() {
+    if (_beatInternal == _targetBeat) {
+      int temp = _beatInternal;
+      _beatInternal = 1;
+      return temp;
+    }
+    return _beatInternal++;
+  }
+
+  void pause() {
+    _timer.cancel();
+  }
+
+  bool updateProperty({
+    int? bpm,
+    int? targetBeat,
+  }) {
+    try {
+      if (bpm != null) {
+        _bpm = bpm;
+      }
+      if (targetBeat != null) {
+        _targetBeat = targetBeat;
+      }
+    } on Exception {
+      return false;
+    }
+    return true;
+  }
 }
-
-class BpmMeterImpl extends BpmMeter {
-  BpmMeterImpl(int beat)
-      : super(
-          beat: beat,
-          bpmStream: Stream.empty(),
-        );
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is BpmMeterImpl &&
-          runtimeType == other.runtimeType &&
-          beat == other.beat;
-}
-*/
