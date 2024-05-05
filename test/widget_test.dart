d8fc70d96b97f32d12c695ddae5211b737a455e5
junsuPark <krpjs0508@gmail.com>
2024-05-05 21:44:52 +0900

    test package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:metronome/main.dart';
+import 'package:metronome/ui/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
